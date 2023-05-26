49753e3acf58287037b98b2417b85522f0fee847
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-05-25 17:12:11 -0700

    Create Inital Crud conf

--- helloworldaws/CRUD/views.py
+++ helloworldaws/CRUD/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+    template_name = 'post_confirm_delete.html'
--- helloworldaws/helloworldaws/settings.py
+++ helloworldaws/helloworldaws/settings.py
@@ -26,11 +26,12 @@ SECRET_KEY = 'django-insecure-10&lvv7o7k94!!o12g0fko)rz=7sml0io9!a=9)eb17$m(z%v=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['helloworld-env.eba-dasmkpze.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['helloworld-env.eba-dasmkpze.us-west-2.elasticbeanstalk.com', 'localhost']
 
 # Application definition
 
 INSTALLED_APPS = [
+    'CRUD',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
