11602fbb847630996ee77e6b88c1c69cdd4b78aa
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-05-26 08:25:26 -0700

    migrate to Data

--- helloworldaws/CRUD/models.py
+++ helloworldaws/CRUD/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    class Meta:
-        app_label = 'CRUD'
 
 class Data(models.Model):
     name = models.CharField(max_length=200)
