ad1ad0b1e2c1ccf930a2b69ad90645666dac775e
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-06 00:42:14 -0700

    rm name

--- prototype/settings.py
+++ prototype/settings.py
@@ -79,7 +79,6 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
