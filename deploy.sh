5c49c09900e477ffed4ec1b8d3986f5124288002
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-06 23:04:58 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ sudo git pull origin master
 sudo pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
 nohup python3 ./Customer/listener.py &
