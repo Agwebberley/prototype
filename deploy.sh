e4fb02e02fb6d4ee9e2fd4fdb93ed12870fd9bab
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-07 17:41:00 -0700

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
-#!/bin/sh     
-sudo git pull origin master
-sudo pip3 install -r requirements.txt
+#!/bin/sh
 python3 manage.py makemigrations
 python3 manage.py migrate
-sudo systemctl restart nginx
-sudo systemctl restart gunicorn
 nohup python3 ./Customer/listener.py &
 gunicorn --bind 0.0.0.0:8000 prototype.wsgi:application
