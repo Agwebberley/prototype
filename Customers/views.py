503d7888d14251a9a98fd06f345813ee89cf63db
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-08 16:56:26 -0700

    Urls and Views

--- Customers/views.py
+++ Customers/views.py
@@ -47,4 +47,55 @@ class LogView(ListView):
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
-    
+
+# Items
+class ItemListView(ListView):
+    model = Items
+    template_name = 'items.html'
+
+class ItemCreateView(CreateView):
+    model = Items
+    form_class = ItemForm
+    template_name = 'item_form.html'
+
+    def form_invalid(self, form):
+        return JsonResponse(form.errors, status=400)
+
+    def get_success_url(self):
+        return reverse_lazy('item_list')
+
+class ItemUpdateView(UpdateView):
+    model = Items
+    form_class = ItemForm
+    template_name = 'item_form.html'
+
+class ItemDeleteView(DeleteView):
+    model = Items
+    success_url = reverse_lazy('item_list')
+    template_name = 'item_confirm_delete.html'
+
+# Orders
+class OrderListView(ListView):
+    model = Orders
+    template_name = 'orders.html'
+
+class OrderCreateView(CreateView):
+    model = Orders
+    form_class = OrderForm
+    template_name = 'order_form.html'
+
+    def form_invalid(self, form):
+        return JsonResponse(form.errors, status=400)
+
+    def get_success_url(self):
+        return reverse_lazy('order_list')
+
+class OrderUpdateView(UpdateView):
+    model = Orders
+    form_class = OrderForm
+    template_name = 'order_form.html'
+
+class OrderDeleteView(DeleteView):
+    model = Orders
+    success_url = reverse_lazy('order_list')
+    template_name = 'order_confirm_delete.html'
