8c06b94b3be0f010d49b170dda540c70fd1a7ebd
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-09 14:21:47 -0700

    Add app_name

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import OrderListView, OrderCreateView, OrderUpdateView, OrderDeleteView
 
+app_name = 'orders'
+
 urlpatterns = [
     path('', OrderListView.as_view(), name='order_list'),
     path('create/', OrderCreateView.as_view(), name='order_create'),
