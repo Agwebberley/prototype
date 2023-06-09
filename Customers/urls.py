3516f66b20cadf8c33e8c35b40a5bd55c15bffde
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-09 14:21:24 -0700

    customer_list was missing

--- Customers/urls.py
+++ Customers/urls.py
@@ -7,6 +7,7 @@ from .views import CustomerListView, CustomerCreateView, CustomerUpdateView, Cus
 app_name = 'customers'
 
 urlpatterns = [
+    path('', CustomerListView.as_view(), name='customer_list'),
     path('create/', CustomerCreateView.as_view(), name='customer_create'),
     path('<int:pk>/update/', CustomerUpdateView.as_view(), name='customer_update'),
     path('<int:pk>/delete/', CustomerDeleteView.as_view(), name='customer_delete')
