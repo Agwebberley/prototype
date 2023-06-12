8be3232d72bbf107b262b12c939107e7c438e1ef
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-11 22:06:26 -0700

    Views & template base

--- AccountsReceivable/forms.py
+++ AccountsReceivable/forms.py
@@ -1,7 +1,3 @@
 from django import forms
 from .models import AccountsReceivable
 
-class AccountsReceivableForm(forms.ModelForm):
-    class Meta:
-        model = AccountsReceivable
-        fields = ('order', 'amount', 'due_date')
--- AccountsReceivable/views.py
+++ AccountsReceivable/views.py
@@ -1,40 +1,60 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
-from django.http import JsonResponse
-from .models import AccountsReceivable
-from .forms import AccountsReceivableForm
+from .models import AccountsReceivable, AccountsReceivableHistory, AccountsReceivablePayment
 
 # Accounts Receivable
 class AccountsReceivableListView(ListView):
     model = AccountsReceivable
     template_name = 'accounts_receivable.html'
 
-class AccountsReceivableCreateView(CreateView):
-    model = AccountsReceivable
-    form_class = AccountsReceivableForm
-    template_name = 'accounts_receivable_form.html'
-
-    def form_invalid(self, form):
-        return JsonResponse(form.errors, status=400)
-
-    def get_success_url(self):
-        return reverse_lazy('accounts_receivable_list')
-
-class AccountsReceivableUpdateView(UpdateView):
-    model = AccountsReceivable
-    form_class = AccountsReceivableForm
-    template_name = 'accounts_receivable_form.html'
-
-class AccountsReceivableDeleteView(DeleteView):
-    model = AccountsReceivable
-    success_url = reverse_lazy('accounts_receivable_list')
-    template_name = 'accounts_receivable_confirm_delete.html'
-
-class AccountsReceivableTogglePaidView(View):
+class AccountTogglePaidView(View):
     def post(self, request, *args, **kwargs):
         accounts_receivable_id = kwargs.get('pk')
-        accounts_receivable = get_object_or_404(AccountsReceivable, pk=accounts_receivable_id)
-        accounts_receivable.paid = not accounts_receivable.paid
-        accounts_receivable.save()
-        return reverse_lazy('accounts_receivable_list')
+        # Create a payment entry
+        payment = AccountsReceivablePayment()
+        payment.accounts_receivable = get_object_or_404(AccountsReceivable, pk=accounts_receivable_id)
+        if not payment.accounts_receivable.paid:
+            payment.amount = payment.accounts_receivable.amount - payment.accounts_receivable.amount_paid
+        else:
+            payment.amount = -payment.accounts_receivable.amount_paid
+        payment.save()
+        return reverse_lazy('accountsreceivable:accounts_receivable_list')
+
+class AccountAddPayment(CreateView):
+    model = AccountsReceivablePayment
+    fields = ['amount']
+    template_name = 'accounts_receivable_payment.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['accounts_receivable'] = get_object_or_404(AccountsReceivable, pk=self.kwargs.get('pk'))
+        return context
+
+    def form_valid(self, form):
+        form.instance.accounts_receivable = get_object_or_404(AccountsReceivable, pk=self.kwargs.get('pk'))
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('accountsreceivable:accounts_receivable_list')
+
+class AccountChangeDueDateView(UpdateView):
+    model = AccountsReceivable
+    fields = ['due_date']
+    template_name = 'accounts_receivable_due_date.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accountsreceivable:accounts_receivable_list')
+
+class AccountDetailView(ListView):
+    model = AccountsReceivableHistory
+    template_name = 'accounts_receivable_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['accounts_receivable'] = get_object_or_404(AccountsReceivable, pk=self.kwargs.get('pk'))
+        return context
+
+    def get_queryset(self):
+        accounts_receivable = get_object_or_404(AccountsReceivable, pk=self.kwargs.get('pk'))
+        return AccountsReceivableHistory.objects.filter(accounts_receivable=accounts_receivable)
