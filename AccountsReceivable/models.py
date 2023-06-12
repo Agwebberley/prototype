ba7cddcc8e13a273247ee578130b6566c2876f13
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-11 21:18:42 -0700

    Add amount_paid and paid_date

--- AccountsReceivable/models.py
+++ AccountsReceivable/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.urls import reverse
 from Orders.models import Orders
+from Customers.models import Customers
 import datetime
 
 class AccountsReceivable(models.Model):
     order = models.ForeignKey(Orders, on_delete=models.CASCADE)
-    amount = models.FloatField()
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     # Due date defaults to 30 days from now
     due_date = models.DateField(default=datetime.date.today() + datetime.timedelta(days=30))
     paid = models.BooleanField(default=False)
+    paid_date = models.DateField(null=True, blank=True)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    
     class Meta:
         app_label = 'AccountsReceivable'
     
