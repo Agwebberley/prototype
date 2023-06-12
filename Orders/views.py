77420605350f8f5130b43ecfdc782257668ec6e0
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-11 19:46:36 -0700

    Create templates and views

--- Orders/views.py
+++ Orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.http import JsonResponse
-from .models import Orders
+from .models import Orders, OrderItem
 from .forms import OrderForm
 
 # Orders
@@ -30,3 +30,16 @@ class OrderDeleteView(DeleteView):
     model = Orders
     success_url = reverse_lazy('order_list')
     template_name = 'order_confirm_delete.html'
+
+# Order Items
+class OrderDetailView(ListView):
+    model = OrderItem
+    template_name = 'order_details.html'
+
+class OrderItemCreateView(CreateView):
+    model = OrderItem
+    fields = ['order', 'item', 'quantity']
+    template_name = 'order_item_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('order_details', kwargs={'pk': self.kwargs['pk']})
