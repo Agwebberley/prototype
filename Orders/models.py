01217e405ee12407f4f352219f08e169d301ddb0
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-11 19:42:55 -0700

    Create OrderItem

--- Orders/models.py
+++ Orders/models.py
@@ -6,13 +6,25 @@ from Items.models import Items
 
 class Orders(models.Model):
     customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
-    item = models.ForeignKey(Items, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     class Meta:
         app_label = 'Orders'
     
     def __str__(self):
         return self.customer
+    
+    def get_absolute_url(self):
+        return reverse('order_list')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    class Meta:
+        app_label = 'Orders'
+    
+    def __str__(self):
+        return self.order
     
     def get_absolute_url(self):
         return reverse('order_list')
