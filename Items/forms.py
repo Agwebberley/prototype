cd4943f216190b2bdb32dc7506700439997ec72e
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-15 22:37:55 -0700

    Items now use reusable templates

--- Items/forms.py
+++ Items/forms.py
@@ -5,3 +5,11 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Items
         fields = ('name', 'description', 'price', 'target_inv', 'current_inv', 'reorder_level')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'description': forms.TextInput(attrs={'class': 'w-full px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'price': forms.NumberInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'target_inv': forms.NumberInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'current_inv': forms.NumberInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'reorder_level': forms.NumberInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'})
+        }
--- Items/views.py
+++ Items/views.py
@@ -8,12 +8,30 @@ from .forms import ItemForm
 # Items
 class ItemListView(ListView):
     model = Items
-    template_name = 'items.html'
+    template_name = 'listview.html'
+
+    # Set model_fields to the fields of the model
+    model_fields = [field.name for field in Items._meta.get_fields()]
+    model_fields.remove('orderitem')
+    model_fields.remove('inventory')
+    model_fields.remove('inventoryhistory')
+    model_fields.remove('bin')
+
+    patterns = {'Update': 'items:item_update', 'Delete': 'items:item_delete'}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_fields'] = self.model_fields
+        context['patterns'] = (self.patterns)
+        context['h1'] = 'Items'
+        context['bpattern'] = 'items:item_create'
+        context['bname'] = 'Create Item'
+        return context
 
 class ItemCreateView(CreateView):
     model = Items
     form_class = ItemForm
-    template_name = 'item_form.html'
+    template_name = 'form.html'
 
     def form_invalid(self, form):
         return JsonResponse(form.errors, status=400)
@@ -24,10 +42,17 @@ class ItemCreateView(CreateView):
 class ItemUpdateView(UpdateView):
     model = Items
     form_class = ItemForm
-    template_name = 'item_form.html'
+    template_name = 'form.html'
     success_url = reverse_lazy('items:item_list')
 
 class ItemDeleteView(DeleteView):
     model = Items
     success_url = reverse_lazy('items:item_list')
-    template_name = 'item_confirm_delete.html'
+    template_name = 'delete.html'
+
+    def get_context_data(self, **kwargs):
+        object = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['object_name'] = 'Item ' + str(object.pk)
+        context['pattern'] = 'items:item_list'
+        return context
