5bf7b464a4d5583921a6e4c7adf5bcda8d0f50fc
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-15 22:28:16 -0700

    Update Customers to start using new templates

--- Customers/forms.py
+++ Customers/forms.py
@@ -5,4 +5,11 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customers
         fields = ('name', 'billing_address', 'shipping_address', 'phone', 'email')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'billing_address': forms.TextInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'shipping_address': forms.TextInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'phone': forms.TextInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'}),
+            'email': forms.EmailInput(attrs={'class': 'w-half px-3 py-2 mb-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none sm:text-sm text-black'})
+        }
 
--- Customers/views.py
+++ Customers/views.py
@@ -1,5 +1,6 @@
 # This is a set of Django views for creating, updating, and deleting instances of a Customer model, with a
 # list view to display all instances.
+from typing import Any
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
@@ -8,7 +9,7 @@ from .forms import CustomerForm
 
 class CustomerListView(ListView):
     model = Customers
-    template_name = 'customers.html'
+    template_name = 'listview.html'
 
     # Set model_fields to the fields of the model
     model_fields = [field.name for field in Customers._meta.get_fields()]
@@ -24,7 +25,7 @@ class CustomerListView(ListView):
 class CustomerCreateView(CreateView):
     model = Customers
     form_class = CustomerForm
-    template_name = 'customer_form.html'
+    template_name = 'form.html'
 
     def form_invalid(self, form):
         return JsonResponse(form.errors, status=400)
@@ -35,9 +36,15 @@ class CustomerCreateView(CreateView):
 class CustomerUpdateView(UpdateView):
     model = Customers
     form_class = CustomerForm
-    template_name = 'customer_form.html'
+    template_name = 'form.html'
 
 class CustomerDeleteView(DeleteView):
     model = Customers
     success_url = reverse_lazy('customers:customer_list')
-    template_name = 'customer_confirm_delete.html'
+    template_name = 'delete.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_name'] = 'Customer ' + str(self.object.pk)
+        context['pattern'] = 'customers:customer_list'
+        return context
