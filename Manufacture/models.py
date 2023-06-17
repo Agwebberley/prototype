0ccdcfc9ceecaa575963762f61339a317e8c8124
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-17 15:51:52 -0700

    Manufactureurls views and ManufactureHistory model

--- Manufacture/models.py
+++ Manufacture/models.py
@@ -14,3 +14,20 @@ class Manufacture(models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('manufacture:manufacture_list')
+
+class ManufactureHistory(models.Model):
+    manufacture = models.IntegerField(null=True, blank=True)
+    item = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.item} - {self.quantity}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('manufacture:manufacture_list')
+    
+    def save(self, *args, **kwargs):
+        if self.manufacture:
+            self.item = Manufacture.objects.get(id=self.manufacture).item.id
+        super().save(*args, **kwargs)
--- Manufacture/urls.py
+++ Manufacture/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from .views import ManufactureList, ManufactureCreate, ManufactureUpdate
 
 urlpatterns = [
+    path('', ManufactureList.as_view(), name='manufacture_list'),
+    path('create/', ManufactureCreate.as_view(), name='manufacture_create'),
+    path('update/<int:pk>/', ManufactureUpdate.as_view(), name='manufacture_update'),
 ]
