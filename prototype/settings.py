3e7e9eaced6f010fd47bee53c5b11c5e01bff7b8
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-06-28 20:39:07 -0700

    Create BPMN app

--- prototype/settings.py
+++ prototype/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'Inventory',
     'Manufacture',
     'AccountsReceivable',
+    'viewflow',
+    'viewflow.frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
