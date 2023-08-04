13a37295ca349cefc615e7e88cb94322661a0e89
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2023-08-04 16:25:58 +0000

    Listeners kinda working

--- Listeners/flow.py
+++ Listeners/flow.py
@@ -1,10 +1,11 @@
 from prefect import Flow, Task
-from .listeners import BaseSQSListener
+from listeners import BaseSQSListener
 import boto3
+
 
 @Task
 class GetQueueUrlTask():
-    def run(self, listener):
+    def run(self):
         Queues = boto3.client("sqs").list_queues()
         return Queues["QueueUrls"]
 
@@ -16,15 +17,17 @@ class StartListenerTask():
 class StopListenerTask():
     def run(self, listener):
         listener.join()  # Or however you decide to implement stopping the listener
+
 @Flow
 def StartListeners():
     listeners = []
     Queues = GetQueueUrlTask().run()
     QueueNames = [queue_url.split("/")[-1] for queue_url in Queues]
+    print(QueueNames)
     for ListenerClass in BaseSQSListener.__subclasses__():
         # Each ListenerClass should be named after the queue it listens to
         # in the format <QueueName>Listener
-        if ListenerClass.__name__[:-8] not in QueuesNames:
+        if ListenerClass.__name__[:-8] not in QueueNames:
             raise ValueError(f"ListenerClass {ListenerClass.__name__} does not have a corresponding queue")
         listener = ListenerClass(queue_url=Queues[QueueNames.index(ListenerClass.__name__[:-8])])
         StartListenersTask(listener=listener).run()
